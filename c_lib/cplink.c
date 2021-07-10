7e48826d0182a5255e11a1c9bad2f0e091100f3c
xiazhu <1994623194@qq.com>
2021-07-10 14:49:50 +0800

    modify

--- c_lib/cplink.c
+++ c_lib/cplink.c
@@ -120,14 +120,13 @@ int main()
 	Ndata data0=add_data('a',5,a);
 	int b[]={5,4,1,4,2,0};
 	Ndata data1=add_data('b',3,b);
-	int c[]={5,1,4,2,9,3,4,0};
+	int c[]={5,6,4,2,3,2,0};
 	Ndata data2=add_data('c',3,c);
 	cNode Node0=createNode(data0);
 	cNode Node1=createNode(data1);
 	cNode Node2=createNode(data2);
 	Node0.next[0]=Node1;
 	Node1.next[0]=Node2;
-	
 //printf("%f\r\n",a[0]);
 //printf("%d\r\n",data0->I[2]);
 /*
@@ -139,5 +138,8 @@ int main()
 */
 //printf("I= %d\r\n",KCL(Node0));
 printallNode(Node0);
+free(data0);
+free(data1);
+free(data2);
 return 0;
 }
